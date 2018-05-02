92731789e258153d000147275887dc859b297547
Ronald Gaminde <ronaldgaminde@Ronalds-Mac.local>
2018-05-01 20:35:00 -0400

    Initial Commit

---
+++ ViewController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+using UIKit;
+
+namespace BMIIOSDevProject
+{
+    public partial class ViewController : UIViewController
+    {
+        partial void Clear_TouchUpInside(UIButton sender)
+        {
+            height.Text = "";
+            weight.Text = "";
+            result.Text = "";
+            category.Text = "";
+            inch.Text = "";
+            severe.TextColor = UIColor.Black;
+            severe1.TextColor = UIColor.Black;
+            moderate.TextColor = UIColor.Black;
+            moderate2.TextColor = UIColor.Black;
+            mild.TextColor = UIColor.Black;
+            mild2.TextColor = UIColor.Black;
+            normal.TextColor = UIColor.Black;
+            normal2.TextColor = UIColor.Black;
+            over.TextColor = UIColor.Black;
+            over2.TextColor = UIColor.Black;
+            obe1.TextColor = UIColor.Black;
+            obec1.TextColor = UIColor.Black;
+            obe2.TextColor = UIColor.Black;
+            obec2.TextColor = UIColor.Black;
+            obe3.TextColor = UIColor.Black;
+            obec3.TextColor = UIColor.Black;
+        }
+
+        partial void Compute_TouchUpInside(UIButton sender)
+        {
+            double ft = Convert.ToDouble(height.Text);
+            double lbs = Convert.ToDouble(weight.Text);
+            double inches = Convert.ToDouble(inch.Text);
+            double meters = Convert.ToDouble(ft * 0.3048);
+            double kilograms = Convert.ToDouble(lbs * 0.454);
+            double inchToMeters = Convert.ToDouble(inches * 0.0254);
+            double mtrs = meters + inchToMeters;
+
+
+            double BMI = kilograms / (mtrs * mtrs);
+            result.Text = Convert.ToString(BMI);
+            if (BMI < 16){
+                category.Text = "Severe Thinness";
+                severe.TextColor = UIColor.Blue;
+                severe1.TextColor = UIColor.Blue;
+            }
+            else if(BMI < 17){
+                category.Text = "Moderate Thinness";
+                moderate.TextColor = UIColor.Blue;
+                moderate2.TextColor = UIColor.Blue;
+            }
+            else if(BMI < 18.5){
+                category.Text = "Mild Thinness";
+                mild.TextColor = UIColor.Blue;
+                mild2.TextColor = UIColor.Blue;
+            }
+            else if (BMI < 25){
+                category.Text = "Normal";
+                normal.TextColor = UIColor.Green;
+                normal2.TextColor = UIColor.Green;
+            }
+            else if (BMI < 30){
+                category.Text = "Overweight";
+                over.TextColor = UIColor.Green;
+                over2.TextColor = UIColor.Green;   
+            }
+            else if (BMI < 35){
+                category.Text = "Obese Class 1";
+                obe1.TextColor = UIColor.Red;
+                obec1.TextColor = UIColor.Red;
+            }
+            else if (BMI < 40){
+                category.Text = "Obese Class 2";
+                obe2.TextColor = UIColor.Red;
+                obec2.TextColor = UIColor.Red;
+            }
+            else {
+                category.Text = "Obese Class 3";
+                obe3.TextColor = UIColor.Red;
+                obec3.TextColor = UIColor.Red;
+            }
+
+        }
+
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,234 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace BMIIOSDevProject
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel category { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton clear { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton compute { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField height { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField inch { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel mild { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel mild2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel moderate { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel moderate2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel normal { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel normal2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel obe1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel obe2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel obe3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel obec1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel obec2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel obec3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel over { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel over2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel result { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel severe { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel severe1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField weight { get; set; }
+
+        [Action ("Clear_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Clear_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Compute_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Compute_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (category != null) {
+                category.Dispose ();
+                category = null;
+            }
+
+            if (clear != null) {
+                clear.Dispose ();
+                clear = null;
+            }
+
+            if (compute != null) {
+                compute.Dispose ();
+                compute = null;
+            }
+
+            if (height != null) {
+                height.Dispose ();
+                height = null;
+            }
+
+            if (inch != null) {
+                inch.Dispose ();
+                inch = null;
+            }
+
+            if (mild != null) {
+                mild.Dispose ();
+                mild = null;
+            }
+
+            if (mild2 != null) {
+                mild2.Dispose ();
+                mild2 = null;
+            }
+
+            if (moderate != null) {
+                moderate.Dispose ();
+                moderate = null;
+            }
+
+            if (moderate2 != null) {
+                moderate2.Dispose ();
+                moderate2 = null;
+            }
+
+            if (normal != null) {
+                normal.Dispose ();
+                normal = null;
+            }
+
+            if (normal2 != null) {
+                normal2.Dispose ();
+                normal2 = null;
+            }
+
+            if (obe1 != null) {
+                obe1.Dispose ();
+                obe1 = null;
+            }
+
+            if (obe2 != null) {
+                obe2.Dispose ();
+                obe2 = null;
+            }
+
+            if (obe3 != null) {
+                obe3.Dispose ();
+                obe3 = null;
+            }
+
+            if (obec1 != null) {
+                obec1.Dispose ();
+                obec1 = null;
+            }
+
+            if (obec2 != null) {
+                obec2.Dispose ();
+                obec2 = null;
+            }
+
+            if (obec3 != null) {
+                obec3.Dispose ();
+                obec3 = null;
+            }
+
+            if (over != null) {
+                over.Dispose ();
+                over = null;
+            }
+
+            if (over2 != null) {
+                over2.Dispose ();
+                over2 = null;
+            }
+
+            if (result != null) {
+                result.Dispose ();
+                result = null;
+            }
+
+            if (severe != null) {
+                severe.Dispose ();
+                severe = null;
+            }
+
+            if (severe1 != null) {
+                severe1.Dispose ();
+                severe1 = null;
+            }
+
+            if (weight != null) {
+                weight.Dispose ();
+                weight = null;
+            }
+        }
+    }
+}
